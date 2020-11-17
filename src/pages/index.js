bed01ea8ba9c919a441a83c690853950aa11c47a
mark <yabut@Kijijis-MacBook-Pro.local>
2020-11-16 21:15:39 -0500

    update quotes

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,13 +30,44 @@ const Button = styled(Link)`
     top: 1px;
   }
 `
+const Quotes = styled.div`
+  font-style: italic;
+`
 
 export default function UsersList() {
   return (
     <Layout>
-      <h1>Pomodoro Clock</h1>
-      <p>commit to something or be distracted by everything</p>
+      <h1>Pomodoro</h1>
       <Button to="/app">Start</Button>
+      <Quotes>
+        <p>"life is a game, level up"</p>
+        <p>"value of time itself is dictated by how well it is used"</p>
+        <p>"commit to something or be distracted by everything"</p>
+        <p>"track the past, organize the present, design the future"</p>
+        <p>
+          "make it obvious, make it attractive, make it easy, make it
+          satisfying"
+        </p>
+        <p>"start by doing it badly, but do it"</p>
+        <p>
+          "you do not rise to the level of your goals, you fall to the levels of
+          your systems"
+        </p>
+        <p>"competence is highly dependent on context"</p>
+        <p>
+          "work expands so as to fill the time available for its completion"
+        </p>
+        <p>"our brain is for having ideas, not storing them"</p>
+        <p>"it’s not about the destination, it’s about the journey"</p>
+        <p>"pace yourself and improve your pace"</p>
+        <p>"life is both a marathon and a race"</p>
+        <p>"time is the greatest editor"</p>
+        <p>
+          "we should always allow some time to elapse, for time discloses the
+          truth"
+        </p>
+        <p>"maximize desirables and minimize undesirables"</p>
+      </Quotes>
     </Layout>
   )
 }
