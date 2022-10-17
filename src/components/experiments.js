15facb44ee348852a978a071e389080f78981c4b
mdotyabut <mark.y@live.com>
2022-10-17 18:52:38 -0400

    add cooler colo"u"rs

--- src/components/experiments.js
+++ src/components/experiments.js
@@ -4,13 +4,13 @@ import styled from "styled-components"
 import './experiments.css';
 
 const StyledDivLight = styled.div`
-  background-color: white;
+  background-image: linear-gradient(#2193b0,#6dd5ed);
   color: black;
   border-radius: 15px;
 `;
 
 const StyledDivDark = styled.div`
-  background-color: black;
+  background-image: linear-gradient(#2b5876,#4e4376);
   color: white;
   border-radius: 15px;
 `;
